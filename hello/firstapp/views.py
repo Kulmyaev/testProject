b27d7cd40c37beb6c6a60afdf5127f1ec537b473
Kulmyaev <inkulmyaev@gmail.com>
2024-03-23 10:48:55 +0300

    added to urls.py :about contact admin and views.py :index about contact

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("<h2>Главная страница</h2")
+
+
+def about(request):
+    return HttpResponse("<h2>О сайте</h2>")
+
+
+def contact(request):
+    return HttpResponse("<h2>Контакты</h2>")
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from firstapp import views
+
 
 urlpatterns = [
+    path('', views.index, name='home'),
+    path('about', views.about),
+    path('contact', views.contact),
     path('admin/', admin.site.urls),
 ]
